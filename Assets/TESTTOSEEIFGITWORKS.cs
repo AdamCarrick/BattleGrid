ca27d8703f0543378fada16b3ffcc67ebefdab30
Adam Glendon Carrick <c07716567@mydit.ie>
2014-03-08 13:01:13 +0000

    Still playing with git
    Added a Script to project to test

---
+++ Assets/TESTTOSEEIFGITWORKS.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TESTTOSEEIFGITWORKS : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		for(int i=0;i<100;i++)
+		{
+			Debug.Log("Fuck Git");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
